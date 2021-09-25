a80acf5274c50f6492cabf28d088f1b0565a6050
Bogdan Martyshevskiy <vigoroous@gmail.com>
2021-09-25 07:41:56 +0300

    Created: product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductManager(models.Manager):
+    def name_sort(self):
+        return self.order_by('-name')
+
+    def category_sort(self):
+        return self.order_by('-category')
+
+    def rating_sort(self):
+        return self.order_by('-rating')
+
+
+class ProductDetails(models.Model):
+    description = models.TextField()
+    composition = models.TextField()
+    contraindication = models.TextField()
+    usage = models.TextField()
+    elimination = models.TextField()
+
+
+class Product(models.Model):
+    objects = ProductManager()
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=24)
+    imageUrl = models.URLField(max_length=200)
+    rating = models.IntegerField(default=0)
+    details = models.ForeignKey(
+        ProductDetails, blank=True, null=True, on_delete=models.CASCADE)
