fd0dbf0bdcc4572550d92a05d188b5b81274eed7
Bogdan Martyshevskiy <vigoroous@gmail.com>
2021-10-16 18:22:55 +0300

    Setup environment variables with django-environ

--- arkadavet/settings.py
+++ arkadavet/settings.py
@@ -10,17 +10,30 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+import environ
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Set the project base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# False if not in os.environ because of casting above
+DEBUG = env('DEBUG')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dt_m$-8angde6obqxb0h$#598f@&bi+p!50as)*jumzngig4_m'
+# Raises Django's ImproperlyConfigured
+# exception if SECRET_KEY not in os.environ
+SECRET_KEY = env('SECRET_KEY') or 'uj2^c^8vzn*k%kb#zj+q$c=p^4q)up@rlr2+q33lcx#7sa-=4t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,7 +94,7 @@ WSGI_APPLICATION = 'arkadavet.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': env('DATABASE_NAME') or str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
